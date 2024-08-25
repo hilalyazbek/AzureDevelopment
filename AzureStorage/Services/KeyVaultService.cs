44cbfe16cf0b9cf6ed02c289a03e479c3d52ce66
Hilal Yazbek <hyazbek@psa.gov.qa>
2024-08-25 13:43:48 +0300

    Added keyvault tutorial

---
+++ AzureStorage/Services/KeyVaultService.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace AzureDevelopment.Services;
+
+public class KeyVaultService
+{
+    private readonly IConfiguration _configuration;
+    private readonly SecretClient _secretClient;
+
+    public KeyVaultService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+
+        var vaultUrl = _configuration.GetConnectionString("KeyVaultUrl");
+
+        _secretClient = new SecretClient(vaultUri: new Uri(vaultUrl), credential: new DefaultAzureCredential());
+    }
+
+    //internal string GetKeyValue(string name)
+    //{
+
+    //}
+
+    internal string GetSecretValue(string name)
+    {
+        var kvSecret = _secretClient.GetSecret("SQLConnection");
+
+        return kvSecret.Value.Value;
+    }
+}
+
